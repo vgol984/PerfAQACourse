ea1686536c60b8694686dd9b247301894b5f2a1a
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-04-11 18:15:42 +0300

    added Homework_1_3.java

---
+++ src/homework_1/Homework_1_3.java
@@ -0,0 +1,11 @@
+package homework_1;
+
+public class Homework_1_3 {
+    public static void main(String[] args) {
+        int n = 999;
+        int a = n / 100;
+        int b = (n / 10) % 10;
+        int c = n % 10;
+        System.out.println(a + b + c);
+    }
+}
