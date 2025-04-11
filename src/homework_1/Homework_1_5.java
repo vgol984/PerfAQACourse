592eb1883efba8118e51780b98feaa7928688aa2
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-04-11 18:48:10 +0300

    added Homework_1_5.java

---
+++ src/homework_1/Homework_1_5.java
@@ -0,0 +1,11 @@
+package homework_1;
+
+public class Homework_1_5 {
+    public static void main(String[] args) {
+        int q = 458079568;
+        int w = 1379;
+        int div = q / w;
+        int rem = q % w;
+        System.out.println(q + "/" + w + "=" + div + " и " + rem + " в остатке.");
+    }
+}
