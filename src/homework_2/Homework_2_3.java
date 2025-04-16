6daddfd87b19617dcc2f538222b3273b8e8cfe5a
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-04-16 14:04:37 +0300

    added Homework_2_3.java

--- src/homework_2/Homework_2_3.java
+++ src/homework_2/Homework_2_3.java
@@ -3,7 +3,7 @@ package homework_2;
 public class Homework_2_3 {
 
     public static void main(String[] args) {
-        for (int i = 10; i <= 20 ; i++) {
+        for (int i = 10; i <= 20; i++) {
             System.out.println(i * i);
         }
     }
