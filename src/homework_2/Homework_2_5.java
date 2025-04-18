7d186214b03fa65cf080c0dba674ce8c5b146a15
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-04-18 17:42:21 +0300

    added Homework_2_5.java

--- src/homework_2/Homework_2_5.java
+++ src/homework_2/Homework_2_5.java
@@ -1,4 +1,26 @@
 package homework_2;
 
+import java.util.Scanner;
+
 public class Homework_2_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите натуральное положительное число:");
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            if (a > 0) {
+                int sum = 0;
+                for (int i = 1; i <= a; i++) {
+                    sum += i;
+                }
+                System.out.print("Сумма чисел от 1 до " + a + " равна " + sum);
+            } else {
+                System.out.println("Число должно быть больше нуля");
+            }
+
+        } else {
+            System.out.println("Вы ввели неверное значение");
+        }
+        scanner.close();
+    }
 }
