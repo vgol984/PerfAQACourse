b94da7a42b08a5993e26c8b7a3cf9fa5149884d3
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-04-22 20:29:53 +0300

    added Homework_3_1.java

--- src/homework_3/Homework_3_1.java
+++ src/homework_3/Homework_3_1.java
@@ -1,6 +1,5 @@
 package homework_3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Homework_3_1 {
@@ -8,7 +7,7 @@ public class Homework_3_1 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Пожалуйста, введите целое положительное число и не используйте других значений, потому что я не хочу писать дополнительные проверки: ");
         int a = scanner.nextInt();
-        int [] mass = new int[a];
+        int[] mass = new int[a];
         for (int i = 0; i < a; i++) {
             mass[i] = (int) (Math.random() * 100);
         }
