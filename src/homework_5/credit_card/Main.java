f0e9bbbda68af944821e1c58a3b03f8460fbd107
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-05-11 15:31:49 +0300

    задание по кредитным картам

--- src/homework_5/credit_card/Main.java
+++ src/homework_5/credit_card/Main.java
@@ -5,14 +5,11 @@ public class Main {
         CreditCard creditCard1 = new CreditCard(12345, 1023.50);
         CreditCard creditCard2 = new CreditCard(23456, 78955.50);
         CreditCard creditCard3 = new CreditCard(56767, 10266893.50);
-
         creditCard1.toTopUp(1000);
         creditCard2.toTopUp(2000);
         creditCard3.toWithdraw(10266893);
-
-        System.out.println("Счет карты " + creditCard1.accountNumber + " равен " + creditCard1.currentBalance);
-        System.out.println("Счет карты " + creditCard2.accountNumber + " равен " + creditCard2.currentBalance);
-        System.out.println("Счет карты " + creditCard3.accountNumber + " равен " + creditCard3.currentBalance);
+        creditCard1.getCurrentBalance();
+        creditCard2.getCurrentBalance();
+        creditCard3.getCurrentBalance();
     }
-
 }
