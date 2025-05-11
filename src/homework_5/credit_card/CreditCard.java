a141930470bf879cbcc221ccf169170d35aad97e
Vitaliy Golubnchiy <victormirin@gmail.com>
2025-05-11 15:21:45 +0300

    задание по кредитным картам

---
+++ src/homework_5/credit_card/CreditCard.java
@@ -0,0 +1,21 @@
+package homework_5.credit_card;
+
+public class CreditCard {
+    int accountNumber;
+    double currentBalance;
+
+    public CreditCard(int accountNumber, double currentBalance) {
+        this.accountNumber = accountNumber;
+        this.currentBalance = currentBalance;
+    }
+
+    public void toTopUp(double sum) {
+        currentBalance += sum;
+    }
+    public void toWithdraw(double sum) {
+        currentBalance -= sum;
+    }
+    public void getCurrentBalance() {
+        System.out.println("Ваш текущий баланс равен " + currentBalance);
+    }
+}
